eb6c1446dd232dcd848068a149193ea9cda795c9
gadeparawiki <gadeparawiki@gmail.com>
2025-01-16 04:05:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from pyscript import document
 # シミュレーション回数
 iteration = 100
 
-# 目標の女神の思いの個数
-# URとUTRなどに分かれている場合は分けて指定。例：[150, 100]
-target_desire_list = [60, 100, 100]
-
 # 祈願1回あたりの獲得アイテムと確率
 items = ['book1', 'stone', 'gold', 'book2', 'grace', 'desire', 'ticket']
 probability = [0.24, 0.14, 0.14, 0.23, 0.08, 0.11, 0.06]
@@ -21,9 +17,10 @@ magnification_probability = [0.89, 0.09, 0.02]
 def choice_magnification():
   return np.random.choice(magnification, 1, p=magnification_probability)[0]
 
-# メインロジック
+# エントリポイント
 def calc_pray_count(event):
   # htmlからの入力処理
+  # 目標の女神の思いの個数(URとUTRなどに分かれている場合は分けて指定。例：[150, 100])
   input_text = document.querySelector("#required_desire")
   target_desire_list = [int(d) for d in re.split('[, ]', input_text.value) if d != '']
   print(target_desire_list)
@@ -33,6 +30,7 @@ def calc_pray_count(event):
   # 女神の思いカウンタ
   desire_count = 0
 
+  # メインロジック
   for _ in range(iteration):
     for target_desire in target_desire_list:
       while desire_count < target_desire:
@@ -43,6 +41,7 @@ def calc_pray_count(event):
           desire_count += 2 * choice_magnification()
       desire_count -= target_desire
 
+  # 結果出力
   print(f'{count/iteration}回')
   output_div = document.querySelector("#output")
   output_div.innerText = f'実行結果：{count/iteration}回'
